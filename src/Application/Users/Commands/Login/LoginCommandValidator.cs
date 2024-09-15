37f2bedd74d892a87bf24649fa9bb8baf88d499c
shxnvrd <trickmen47@gmail.com>
2024-09-15 17:42:48 +0500

    Added validators for commands and queries

--- src/Application/Users/Commands/Login/LoginCommandValidator.cs
+++ src/Application/Users/Commands/Login/LoginCommandValidator.cs
@@ -4,24 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
 
-namespace Application.Users.Commands.Registration
+namespace Application.Users.Commands.Login
 {
-    public class RegistrationCommandValidator : AbstractValidator<RegistrationCommand>
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
     {
-        public RegistrationCommandValidator()
+        public LoginCommandValidator()
         {
-            RuleFor(c => c.UserName)
-                .NotEmpty().WithMessage("User name cannot be empty");
-
             RuleFor(c => c.Email)
                 .NotEmpty().WithMessage("Email address is required")
                 .EmailAddress().WithMessage("Invalid email address");
 
-            RuleFor(c => c.PhoneNumber)
-                .NotEmpty().WithMessage("Phone number is required")
-                .Length(10).WithMessage("Invalid phone number");
-
-             RuleFor(c => c.Password)
+            RuleFor(c => c.Password)
                 .NotEmpty().WithMessage("Your password cannot be empty")
                 .MinimumLength(8).WithMessage("Your password length must be at least 8.")
                 .MaximumLength(20).WithMessage("Your password length must not exceed 20.")
